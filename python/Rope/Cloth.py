82984a485b61ad63ca0835036b189530b7509d7a
Patrick <patrickwigley2@gmail.com>
2023-06-18 16:53:15 +0100

    Removed excessive whitespace & some changes

--- python/Rope/Cloth.py
+++ python/Rope/Cloth.py
@@ -1,53 +1,29 @@
-
-
-
-
-
-
 import pygame,sys
 from ClothObjects import Main
 
 width,height = 800, 1000
 screen_size = (width, height)
 
-Frame_Rate = pygame.time.Clock()
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode(screen_size)
 
-
-
-pygame.display.set_caption("Rope Simulation - FOR MONSTER MANIC")
+pygame.display.set_caption("Vine Simulation Example - FOR MONSTER MANIC")
 cloth = Main(10, 15, 10)  # ALTER ROPE: VERTICES QUANTITY, SCALING - (SIZING OF ROPE ENTIRLEY), SPACING 
 
-
-
-
-
 while True:
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
-
     screen.fill((255,255,255))
     
     mouse = pygame.mouse.get_pos()
     
-
     cloth.motion()
     cloth.verlet()
-    
-
-    cloth.set_placement(width)
-     
-    
+    cloth.set_placement(width)    
     cloth.alter(mouse)
     cloth.draw(screen)
 
-
-
-
-
-
-    Frame_Rate.tick(60)
+    clock.tick(60)
     pygame.display.update()
--- python/Rope/ClothObjects.py
+++ python/Rope/ClothObjects.py
@@ -8,32 +8,23 @@ def create_objects(quantity):
     for i in range(quantity):
         my_objects.append(ClothObjects(1.0, 1.0, spacing))
         spacing += 1.1                                          # SOLVED: SPACING CANNOT BE A WHOLE NUMBER - (MUST BE FLOAT)
-
     return my_objects
 
 
 def create_index_connections(quantity):
     '''Sets list for the connections of each vertex. '''
-                                                                        #                  All this list is is the connections which will be looked at in the calculations below. It is basically ( current index & index + 1).
+    #All this list is is the connections which will be looked at in the calculations below. It is basically ( current index & index + 1).
     connections = []                                                                     
     for i in range(quantity - 1):                                    
         connections.append([i, i+1, 5])
 
     return connections
 
-
-
-
-def calculate_hypot(p1X, p1Y, p2X, p2Y):
+def calculate_hypot(p1_x, p1_y, p2_x, p2_y):
     ''' find distance between point1 (X & Y) and point2 (X & Y) '''
-
-    hypotenuse = pymath.sqrt((p1X - p2X)** 2 + (p1Y - p2Y)** 2)
-
-    return hypotenuse
+    return pymath.sqrt((p1_x - p2_x)** 2 + (p1_y - p2_y)** 2)
 
     
-
-
 class ClothObjects():
     def __init__(self, x,y, spacing):
         self.x = x
@@ -46,28 +37,19 @@ class ClothObjects():
         self.origanal_y = y + spacing
 
 
-
-
-
 class Main():
     """Params: Quantity of points;  Scale/Size of points;   Spacing intended; """
     def __init__(self, quantity, scale, spacing):
-        
-
         self.scale = scale
         self.spacing = spacing
-
         self.points = create_objects(quantity)
-
         self.connections = create_index_connections(quantity)
 
 
 
     def motion(self):
         '''Interia'''   
-
         for point in self.points:
-
             velocity_x = point.x - point.old_x
             velocity_y = point.y - point.old_y
 
@@ -78,13 +60,9 @@ class Main():
             point.y += velocity_y
             point.y += 0.05
                                                                       # interia Algorithm
-
-
-    
+  
     def verlet(self):
         ''' Verlet'''
-
-        
         for point in self.connections:
 
             distance = calculate_hypot(self.points[point[0]].x, self.points[point[0]].y, self.points[point[1]].x, self.points[point[1]].y)
@@ -100,13 +78,9 @@ class Main():
             self.points[point[1]].x += difference_x * fix
             self.points[point[1]].y += difference_y * fix
                                                                         # Verlet Algorithm
-                                                                       
-  
-  
-  
+                                                                         
     def set_placement(self, width):
         ''' Places main (top vertex) '''
-
         for i, point in enumerate(self.points):
             if i == 0:
                 point.x = point.origanal_x + (width/2) / self.scale
@@ -114,13 +88,9 @@ class Main():
                 point.old_x = point.x
                 point.old_y = point.y
 
-
-
-
     def alter(self, mouse):
         ''' Param:  [MouseX, MouseY];
-        Alters points if collision'''
-        
+        Alters points if collision'''       
         mx = mouse[0]
         my = mouse[1]
 
@@ -135,20 +105,15 @@ class Main():
     def draw(self, screen):
         ''' Param:  Screen which is being displayed;
         Scales & draws vertices & lines connecting'''
-
-
         points_scaled = []
 
         for size in self.points:
             point_x = size.x * self.scale
             point_y = size.y * self.scale
             points_scaled.append([point_x, point_y])
-
   
         for draw in self.connections:
             pygame.draw.line(screen, (135, 86, 56), (points_scaled[draw[0]][0], points_scaled[draw[0]][1]), (points_scaled[draw[1]][0], points_scaled[draw[1]][1]), 15)
-            
-
             pygame.draw.circle(screen, (0,0,0), (points_scaled[draw[0]][0], points_scaled[draw[0]][1]), 10, 20)
 
 
